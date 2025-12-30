ab870e0eece135332cc408a1fc5bbe3473b5470e
Sarvesh Jathar <sarveshjathar448@gmail.com>
2025-12-30 18:21:10 +0530

    Create views for gold.dim_customers, gold.dim_product, and gold.fact_sales
    Added views for customers, products, and sales in the gold layer.

--- SQL Scripts/script.sql
+++ SQL Scripts/script.sql
@@ -394,6 +394,68 @@ end;
 
 exec silver.load_silver;
 
+--Building golder layer
+
+create or alter view gold.dim_customers as 
+select 
+row_number() over(order by cst_id) as customer_key,
+ci.cst_id as customer_id,
+ci.cst_key as customer_number,
+ci.cst_firstnam as firstname,
+ci.cst_lastname as lastname,
+ci.cst_marital_status as marital_status,
+case when ci.cst_gndr != 'n/a' then ci.cst_gndr
+	 else coalesce('ca.gen', 'n/a')
+end as gender,
+ci.cst_create_date as create_date,
+ca.bdate as birthdate,
+la.cntry as country
+from silver.crm_cust_info as ci
+left join silver.erp_cust_az12 as ca
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 as la
+on ci.cst_key = la.cid
+
+select * from gold.dim_customers;
+
+create or alter view gold.dim_product as 
+select
+row_number() over(order by prd_start_dt) as product_key,
+pn.prd_id as product_id,
+pn.cat_id as category_id,
+pc.cat as category,
+pc.subcat as subcategory,
+pn.prd_key as product_number,
+pn.prd_nm as product_name,
+pn.prd_cost as cost,
+pn.prd_line as product_line,
+pc.maintainance as mainatainance,
+pn.prd_start_dt as start_date
+from silver.crm_prd_info as pn
+left join silver.erp_px_cat_g1v2 as pc
+on pn.cat_id = pc.id
+where pn.prd_end_dt is null
+
+select * from gold.dim_product;
+
+
+create or alter view gold.fact_sales as
+select 
+sd.sls_ord_num as order_number,
+pr.product_key ,
+cu.customer_key,
+sd.sls_order_dt as order_date,
+sd.sls_ship_dt as ship_date,
+sd.sls_due_dt as due_date,
+sd.sls_sales as total_sales,
+sd.sls_quantity as total_quantity,
+sd.sls_price as total_price
+from silver.crm_sales_details as sd
+left join gold.dim_product as pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers as cu
+on sd.sls_cust_id = cu.customer_id
+
 
 
 
